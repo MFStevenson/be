c6caa181c3cb66de0d049724aa67c8970b014aac
Mia Stevenson <mflstevenson105@gmail.com>
2023-11-20 14:29:05 +0000

    completes task 5

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -1,4 +1,7 @@
-const { selectArticleById } = require("../models/articles-model");
+const {
+  selectArticleById,
+  selectArticles,
+} = require("../models/articles-model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
@@ -10,6 +13,14 @@ exports.getArticleById = (req, res, next) => {
       } else {
         res.status(200).send({ article });
       }
+    })
+    .catch(next);
+};
+
+exports.getArticles = (req, res, next) => {
+  selectArticles()
+    .then((articles) => {
+      return res.status(200).send({ articles });
     })
     .catch(next);
 };
