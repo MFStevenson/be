5cf9f92f6584bbc69edfc1fe15408d5092397dfe
Mia Stevenson <mflstevenson105@gmail.com>
2023-12-11 10:03:28 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const {
   handleCustomErrors,
   handleServerErrors,
@@ -10,6 +11,7 @@ const apiRouter = require("./routes/api-router");
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/api", apiRouter);
